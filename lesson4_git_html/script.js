d675217e7b1e9d01cb7953fa50afc8be8999d4d3
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-03-18 21:10:07 +0700

    asdddd

--- lesson4_git_html/script.js
+++ lesson4_git_html/script.js
@@ -1,1 +1,9 @@
-alert("Hello World");
+let number = prompt('Nhập số cần kiểm tra:');
+if (number % 2 === 0 && number != 0) {
+    document.write(number + ' Là số chẵn.');
+}
+else if (number % 2 !==0) {
+    document.write(number + ' Là số lẻ.');
+}
+else {
+    document.write('Invalid input!');}
