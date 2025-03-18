0c1aa2e3f7b31c93eb6471559730845cef8a08f4
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-03-18 21:15:28 +0700

    asdddd

--- lesson4_git_html/tinh_dien_tich_HCN.js
+++
@@ -1,7 +0,0 @@
-let inputWidth;
-let inputHeight;
-inputWidth = prompt("Enter the width");
-
-inputHeight = prompt("Enter the height");
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
